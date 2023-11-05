290959955d2b8f5cbae394b3010003e8f172a4c2
Сюфу Иван <ivan.syufu@casestudio.ru>
2023-11-05 20:38:24 +1000

    homework is ready

--- otus-01/src/otus/homework.clj
+++ otus-01/src/otus/homework.clj
@@ -1,5 +1,12 @@
 (ns otus.homework)
 
 
-(defn solution "Add your solution as fuction body here" [])
+(defn solution "Add your solution as fuction body here" []
+  (let [v1 (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+        v2 (* 3 (- 6 2) (- 2 7))]
+    (double (/ v1 v2))))
+
+(comment
+  (solution))
+
 
