27995c23f2e2dffcdf18079da8c5c0fe5ede9d75
Сюфу Иван <ivan.syufu@casestudio.ru>
2023-11-12 17:10:11 +1000

    small fix

--- otus-02/test/otus_02/homework/fizzbuzz.clj
+++ otus-02/test/otus_02/homework/fizzbuzz.clj
@@ -10,13 +10,13 @@
 - если число делится и на 3 и на 5, выведите 'FizzBuzz'. "
   (map (fn [x]
          (cond
-           (and (zero? (rem x 3)) (zero? (rem x 5))) " FizzBuzz "
-           (zero? (rem x 3)) " Fizz "
-           (zero? (rem x 5)) " Buzz "
+           (and (zero? (rem x 3)) (zero? (rem x 5))) "FizzBuzz"
+           (zero? (rem x 3)) "Fizz"
+           (zero? (rem x 5)) "Buzz"
            :else x))
        (range 1 (inc n))))
 
 
 (deftest fizz-buzz-test
   (is (= (fizz-buzz 10)
-         '(1 2 " Fizz " 4 " Buzz " " Fizz " 7 8 " Fizz " " Buzz "))))
+         '(1 2 "Fizz" 4 "Buzz" "Fizz" 7 8 "Fizz" "Buzz"))))
