c4f2530526574eee6ab37a194cbffdce7190f6f0
Vadzimz <56486120+Vadzimz@users.noreply.github.com>
2023-11-12 20:40:57 +0300

    Update pangram.clj

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,6 +1,4 @@
 (ns otus-02.homework.pangram
   (:require [clojure.string :as string]))
 
-
-(defn is-pangram [test-string])
-
+(defn is-pangram [test-string] (->> test-string string/lower-case (filter #(Character/isLetter %)) (map int) (into #{}) count (= 26)))
