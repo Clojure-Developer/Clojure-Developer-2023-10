23e0285ecbf01a0aa75519e308327732ab99db9b
medvedevvu <medvedevvu72@gmail.com>
2023-11-12 11:20:27 +0300

    HW1 version 1 - formatting done

--- otus-01/src/otus/homework.clj
+++ otus-01/src/otus/homework.clj
@@ -1,16 +1,5 @@
 (ns otus.homework)
 
-
 (defn solution "Add your solution as fuction body here" []
-(let[
-     a (+ 6 (/ 4 5))
-     b (- 3 a)
-     c (- 2 b)
-     d (+ 5 (+ 4 c))
-     e (- 2 7)
-     f (- 6 2)
-     g (* e f)
-     h (* 3 g)
-     r (/ d h)
-    ] (double r)))
-
+  (double (/  (+ 5 (+ 4 (- 2 (- 3 (+ 6 (/ 4 5))))))
+              (* 3 (* (- 2 7) (- 6 2))))))
