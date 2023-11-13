05f42daf9b9b6351ccf48c3c3203e639f9f60358
medvedevvu <medvedevvu72@gmail.com>
2023-11-13 22:22:42 +0300

    HW2 - palindrome done

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,6 +1,7 @@
 (ns otus-02.homework.palindrome
   (:require [clojure.string :as string]))
 
-
-(defn is-palindrome [test-string])
-
+(defn is-palindrome [test-string]
+  (let [normal-str (string/trim (string/upper-case (string/replace test-string #"\W" "")))
+        invert-str (string/reverse (string/trim (string/upper-case (string/replace test-string #"\W" ""))))] (= normal-str invert-str)))
+ 
