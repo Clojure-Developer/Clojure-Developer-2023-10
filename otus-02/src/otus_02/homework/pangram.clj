e1792056c44ef30748eef00de1dbf2ff3795f233
medvedevvu <medvedevvu72@gmail.com>
2023-11-15 22:55:49 +0300

    HW2 pangram done

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,6 +1,12 @@
 (ns otus-02.homework.pangram
   (:require [clojure.string :as string]))
 
-
-(defn is-pangram [test-string])
-
+(defn is-pangram [test-string]
+  (let [alphabet "abcdefghijklmnopqrstuvwxyz"
+        alph-vec (map #(str %) (vec alphabet))
+        normal-string1 (string/lower-case test-string)
+        normal-string (map #(str %)
+                           (vec (filter #(> (.indexOf alphabet (str %1)) -1)
+                                        (vec normal-string1))))
+        chr-in-alpha-list (map #(.indexOf normal-string %)  alph-vec)]
+    (= (count (filter #(= % -1) chr-in-alpha-list)) 0)))
