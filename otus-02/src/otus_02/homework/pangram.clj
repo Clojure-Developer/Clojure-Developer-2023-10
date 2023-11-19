b03f822659ffbce7c50e984783affb884612ba13
medvedevvu <medvedevvu72@gmail.com>
2023-11-19 21:19:01 +0300

    HW2-01 pangram corrected

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,12 +1,19 @@
 (ns otus-02.homework.pangram
   (:require [clojure.string :as string]))
 
+;;(defn is-pangram [test-string]
+;;  (let [alphabet "abcdefghijklmnopqrstuvwxyz"
+;;        alph-vec (map str (vec alphabet))
+;;        normal-string1 (string/lower-case test-string)
+;;        normal-string (map str
+;;                           (filter #(> (.indexOf alphabet (str %1)) -1)
+;;                                   normal-string1))
+;;        chr-in-alpha-list (map #(.indexOf normal-string %)  alph-vec)]
+;;    (= (count (filter #(= % -1) chr-in-alpha-list)) 0)))
+
 (defn is-pangram [test-string]
   (let [alphabet "abcdefghijklmnopqrstuvwxyz"
-        alph-vec (map #(str %) (vec alphabet))
-        normal-string1 (string/lower-case test-string)
-        normal-string (map #(str %)
-                           (vec (filter #(> (.indexOf alphabet (str %1)) -1)
-                                        (vec normal-string1))))
-        chr-in-alpha-list (map #(.indexOf normal-string %)  alph-vec)]
-    (= (count (filter #(= % -1) chr-in-alpha-list)) 0)))
+        normal-string (-> test-string
+                          (string/replace #"[\s\W]" "")
+                          string/lower-case)]
+    (= (set alphabet) (set normal-string))))
