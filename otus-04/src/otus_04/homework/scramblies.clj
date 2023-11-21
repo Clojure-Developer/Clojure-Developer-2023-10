a5dc1e3efdd54bea2c2ffc21cc19471b39c12779
Сюфу Иван <ivan.syufu@casestudio.ru>
2023-11-21 18:18:36 +1000

    ready scramble?

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -4,7 +4,25 @@
 ;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
 
 (defn scramble?
-  "Функция возвращает true, если из букв в строке letters
-  можно составить слово word."
+  "Вариант 1"
   [letters word]
-  nil)
+  (let [
+        f1 (frequencies letters)
+        f2 (frequencies word)
+        f2-size (count f2)true-count
+        true-count (count (for [[k v] f2 :let [y (<= v (get f1 k 0))] :while (true? y)] y))]
+    (= f2-size true-count)))
+
+(defn scramble-v2?
+  "Вариант 2"
+  [letters word]
+  (let [f1 (frequencies letters)
+        f2  (frequencies word)
+        ms (into {} (for [[k v] f2] [k (* -1 v)]))
+        res (empty? (filter #(neg? (second %)) (merge-with + f1 ms)))]
+    res))
+
+(comment
+  (scramble? "katas" "steak")
+
+  (scramble-v2? "katas" "steak"))
