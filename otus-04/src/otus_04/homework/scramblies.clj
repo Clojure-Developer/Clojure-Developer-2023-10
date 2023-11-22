e99a55b12b0d904e37e7fdeca11b827ca96b6b50
Vadzimz <56486120+Vadzimz@users.noreply.github.com>
2023-11-22 15:56:13 +0300

    Update scramblies.clj

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -7,9 +7,6 @@
   "Функция возвращает true, если из букв в строке letters
   можно составить слово word."
   [letters word]
-    (let [f-freq  (fn [x] (->> (set word)
-                               (map #(get (frequencies x) % 0))))
-         [x y]    (->> [letters word]
-                       (map f-freq))]
-      (->> (map >= x y)
-           (reduce #(and %1 %2) true))))
+    (let [f-freq  (fn [x] (map #(get (frequencies x) % 0) (set word)))
+        [x y]   (map f-freq [letters word])]
+          (reduce #(and %1 %2) true (map >= x y))))
