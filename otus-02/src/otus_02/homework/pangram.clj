743cde9ff4feab9cf45f7057ce9f7bfe0a5f9b7c
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-11-22 20:28:17 +0300

    complete pangram task

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,6 +1,17 @@
 (ns otus-02.homework.pangram
   (:require [clojure.string :as string]))
 
+(def alphabet-set
+  (set (map char (range (int \a) (inc (int \z))))))
 
-(defn is-pangram [test-string])
+(defn get-only-alphabets-chars [input-string]
+  (string/replace input-string #"[^A-Za-z]" ""))
 
+(defn normalize-str [input-string]
+  (->> input-string
+       (get-only-alphabets-chars)
+       (string/lower-case)
+       (string/trim)))
+
+(defn is-pangram [test-string]
+  (= alphabet-set (set (normalize-str test-string))))
