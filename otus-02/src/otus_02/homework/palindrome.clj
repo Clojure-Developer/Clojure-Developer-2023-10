c983d14d00deed3488790c37a179e77d2a35b619
Gregory Tereshko <gtrsh@users.noreply.github.com>
2023-11-22 16:34:54 +0300

    complete palindrome task

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,6 +1,15 @@
 (ns otus-02.homework.palindrome
   (:require [clojure.string :as string]))
 
+(defn get-only-alphabets-chars [input-string]
+  (string/replace input-string #"[^A-Za-z]" ""))
 
-(defn is-palindrome [test-string])
+(defn normalize-str [input-string]
+  (->> input-string
+       (get-only-alphabets-chars)
+       (string/lower-case)
+       (string/trim)))
 
+(defn is-palindrome [test-string]
+  (let [normalized-string (normalize-str test-string)]
+    (= normalized-string (string/reverse normalized-string))))
