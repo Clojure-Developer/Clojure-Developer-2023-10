1287555ede2ec49d75e355fb90f2a308febfc9fc
Сюфу Иван <ivan.syufu@casestudio.ru>
2023-11-23 01:30:24 +1000

    ready magic-square

--- otus-04/src/otus_04/homework/magic_square.clj
+++ otus-04/src/otus_04/homework/magic_square.clj
@@ -6,6 +6,18 @@
 ;; Подсказка: используйте "Siamese method"
 ;; https://en.wikipedia.org/wiki/Siamese_method
 
+(defn fill-matrix [n max curr x y matrix]
+  "next"
+  (if (> curr max) matrix
+      (let [new-matrix (assoc-in matrix [x y] curr)
+            new-x (if (zero? x) (dec n) (dec x))
+            new-y (if (= y (dec n)) 0 (inc y))
+            nnew-x (if (zero? (get-in new-matrix [new-x new-y])) new-x (inc x))
+            nnew-y (if (= new-x nnew-x) new-y y)]
+        (println curr x y)
+        (println new-matrix)
+        (fill-matrix n max (inc curr) nnew-x nnew-y new-matrix))))
+
 (defn magic-square
   "Функция возвращает вектор векторов целых чисел,
   описывающий магический квадрат размера n*n,
@@ -14,4 +26,15 @@
   Магический квадрат должен быть заполнен так, что суммы всех вертикалей,
   горизонталей и диагоналей длиной в n должны быть одинаковы."
   [n]
-  [[0]])
+  (fill-matrix n (* n n) 1 0 (int  (/ (dec n) 2)) (vec (repeat n (vec (repeat n 0)))))
+  )
+
+
+(comment
+  (magic-square 3) 
+  (magic-square 1) 
+  (magic-square 5)
+  (fill-matrix 3 9 1 0 1 [[0 0 0] [0 0 0] [0 0 0]])
+  (assoc-in [[0 0 0] [0 0 0] [0 0 0]]  [1 1] 5)
+  )
+
