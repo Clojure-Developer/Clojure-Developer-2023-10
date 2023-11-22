48cbfdbd10019f66bf93547a0cb74f42e2eb4655
Vadzimz <56486120+Vadzimz@users.noreply.github.com>
2023-11-22 16:40:30 +0300

    Update magic_square.clj

--- otus-04/src/otus_04/homework/magic_square.clj
+++ otus-04/src/otus_04/homework/magic_square.clj
@@ -10,15 +10,13 @@
 ;; r = 1..n  MagSq = n * mod(r' + (r - div(n + 3, 2)), n) + mod(r' + r * 2 - 2, n) + 1
 (defn magic-square [n]
   {:pre [(odd? n)]}
-  (let [rng          (->> n 
-                          range 
-                          (map inc))
+  (let [rng          (map inc (range n))
         repeat-rng   (fn [x] (flatten (repeat n x)))
         sum-row-col  (fn [x y] (map + (sort (repeat-rng x)) (repeat-rng y)))
         f-main       (fn [x] (map #(mod % n) (sum-row-col rng x)))
-        b1           (map (partial + (- (quot (+ n 3), 2))) rng)
-        b2           (map (partial #(- (* 2 %) 2)) rng)]
-    (->> (map + (map (partial * n) (f-main b1)) (f-main b2))
+        b1           (map #(- % (quot (+ n 3), 2)) rng)
+        b2           (map #(- (* % 2) 2) rng)]
+    (->> (map + (map #(* % n) (f-main b1)) (f-main b2))
          (map inc)
          (partition n)
          (map vec)
