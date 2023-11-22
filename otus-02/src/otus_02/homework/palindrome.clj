aa186a871e8b7e908aa71769d3972eee4cbb0e54
Vadzimz <56486120+Vadzimz@users.noreply.github.com>
2023-11-22 15:39:56 +0300

    Update palindrome.clj

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,5 +1,8 @@
 (ns otus-02.homework.palindrome
   (:require [clojure.string :as string]))
 
-(defn is-palindrome [test-string] (->> test-string string/lower-case (filter #(Character/isLetter %)) (#(= % (reverse %)))))
+(defn is-palindrome [test-string] 
+  (->> test-string string/lower-case 
+       (filter #(Character/isLetter %)) 
+       (#(= % (reverse %)))))
 
