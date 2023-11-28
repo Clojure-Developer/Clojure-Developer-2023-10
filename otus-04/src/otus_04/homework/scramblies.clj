7822149491936faf7e8dafc1d51ca3dcca50f3e0
Ivan <skylightis666@gmail.com>
2023-11-28 13:48:45 +0300

    HW 3 scramblies done + tests ns refactor

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -3,8 +3,26 @@
 ;; Оригинальная задача:
 ;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
 
+(defn nil-to-zero-wrapper [value]
+    (if (nil? value)
+        0
+        value))
+
+(defn compare-freq [coll1 coll2 letter]
+    (>= (nil-to-zero-wrapper (get coll1 letter))
+        (get coll2 letter)))
+
 (defn scramble?
-  "Функция возвращает true, если из букв в строке letters
-  можно составить слово word."
-  [letters word]
-  nil)
+    "Функция возвращает true, если из букв в строке letters
+    можно составить слово word."
+    [letters word]
+    (let [freq-letters (frequencies letters)
+          freq-word (frequencies word)]
+        (every? identity
+                (map (partial compare-freq freq-letters freq-word) (keys freq-word)))))
+
+(comment
+    (frequencies "asdasde")
+    (get {\a 2, \s 2, \d 2, \e 1} \a)
+    (scramble? "rkqodlw" "word")
+    (>= (get (frequencies "rkqodlw") \w) (get (frequencies "word") \w)))
