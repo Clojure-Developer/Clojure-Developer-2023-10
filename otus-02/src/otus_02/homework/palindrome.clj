22603a016ca0ea5b462f2aeed47926fd73000ad8
Andrey Boriskin <boriskinay@gmail.com>
2023-12-10 16:17:25 +0300

    HW2 Boriskin Andrey done

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -2,5 +2,10 @@
   (:require [clojure.string :as string]))
 
 
-(defn is-palindrome [test-string])
-
+(defn is-palindrome
+  [test-string]
+  (as-> test-string p
+        (re-seq #"[\w+]" p)
+        (string/join "" p)
+        (string/lower-case p)
+        (= (string/reverse p) p))) 
--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -2,5 +2,11 @@
   (:require [clojure.string :as string]))
 
 
-(defn is-pangram [test-string])
-
+(defn is-pangram
+  [test-string]
+  (as-> test-string p
+    (string/lower-case p)
+    (re-seq #"[\w+]" p)
+    (set p)
+    (string/join "" p)
+    (= (count p) 26)))
