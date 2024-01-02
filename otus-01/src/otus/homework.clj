6bb5cdc68131d97a9858e297328f096072dadf66
aleksei.zolotykh <aazolotyh@gmail.com>
2024-01-02 19:46:49 +0300

    Revert "first attempt of homework"
    This reverts commit 4bf434d38e65ae5810b7a2be7e304d2c88ea5571.

--- otus-01/src/otus/homework.clj
+++ otus-01/src/otus/homework.clj
@@ -1,10 +1,5 @@
 (ns otus.homework)
 
-(defn solution "Add your solution as fuction body here" []
-  (double
-   (/
-    (+ 5 4
-       (- 2 (- 3 (+ 6 4/5))))
-    (* 3
-       (- 6 2)
-       (- 2 7)))))
+
+(defn solution "Add your solution as fuction body here" [])
+
