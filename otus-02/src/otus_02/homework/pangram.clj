289084e57f11557fe33072b6b94bf97bba9b7513
aleksei.zolotykh <aazolotyh@gmail.com>
2024-01-02 22:21:55 +0300

    add pangram

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,6 +1,14 @@
 (ns otus-02.homework.pangram
   (:require [clojure.string :as string]))
 
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
 
-(defn is-pangram [test-string])
+(defn is-pangram [test-string]
+
+  (= alphabet
+     (-> test-string
+         (string/lower-case)
+         (string/replace #"[\W]{1,}" "")
+         (set)
+         (string/join))))
 
