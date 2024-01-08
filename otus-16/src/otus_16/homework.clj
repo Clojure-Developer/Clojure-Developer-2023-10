ae18f2857907a9c26cbc2c7f90527f1133a34a34
Сюфу Иван <ivan.syufu@casestudio.ru>
2024-01-08 17:55:16 +1000

    ready

--- otus-16/src/otus_16/homework.clj
+++ otus-16/src/otus_16/homework.clj
@@ -1,21 +1,89 @@
-(ns otus-16.homework)
+(ns otus-16.homework
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.core.reducers :as r])
+  (:import (java.net URLDecoder)))
 
+(def ^:dynamic *logs-dir* "logs")
 
+(defn lines-of-file [file fn]
+  (with-open [rdr (io/reader file)]
+    (fn (line-seq rdr))))
+
+(defn url-decode [s]
+  (java.net.URLDecoder/decode
+   (if (str/includes? s "\\x")
+     (str/replace s #"\\x" "%")
+     s)))
+
+(defn extract-url [s]
+  (let [arr (str/split s #" " 3)
+        url (if (= 1 (count arr))
+              (first arr)
+              (second arr))]
+    (url-decode url)))
+
+(defn parse-line [line url referrer]
+  (let [rgx #"(\d+\.\d+\.\d+\.\d+) (\S+) (\S+) \[(.+?)\] \"(.*?)\" (\d+) (\d+) \"(.*?)\" \"(.*?)\""
+        groups (re-matches rgx line)
+        current-url (extract-url (nth groups 5))
+        bytes (parse-long (nth groups 7))
+        current-referrer (url-decode (nth groups 8))
+        is-referrer (if (= :all referrer)
+                      true
+                      (some?  (str/index-of current-referrer referrer)))]
+    {:bytes-by-url (if (or (= url :all) (= url current-url))
+                     {current-url bytes}
+                     {})
+     :total-bytes bytes
+     :urls-by-referrer (cond
+                         (= referrer :all) {current-referrer 1}
+                         is-referrer {referrer 1}
+                         :else {})}))
+
+(defn deep-merge-with
+  [f & maps]
+  (apply
+   (fn m [& maps]
+     (if (every? map? maps)
+       (apply merge-with m maps)
+       (apply f maps)))
+   maps))
+
+(defn count-bytes-use-redusers [lines url referrer]
+  ;; "Elapsed time: 7621.87414 msecs" for access.log.4 121 mb
+  ;; не так быстро как count-bytes-use-pmap но использует не более 2-3 ядер
+  (r/fold
+   (r/monoid #(deep-merge-with + %1 %2) (constantly {}))
+   (r/map #(parse-line %1 url referrer) lines)))
+
+(defn count-bytes-use-pmap [lines url referrer]
+  ;; "Elapsed time: 2307.441963 msecs" for access.log.4 121 mb
+  ;; быстро но использует все ядра на 100%
+  (reduce #(deep-merge-with + %1 %2) (pmap #(parse-line %1 url referrer) lines)))
+
+(defn solution-file [url referrer file]
+  (lines-of-file file #(count-bytes-use-redusers %1 url referrer)))
 
 (defn solution [& {:keys [url referrer]
                    :or   {url :all referrer :all}}]
-  (println "doing something")
-  {:total-bytes      12345
-   ;; если указан параметр url, то в хэш-мапе будет только одно значение
-   :bytes-by-url     {"some-url" 12345}
-   ;; если указан параметр referrer, то в хэш-мапе будет только одно значение
-   :urls-by-referrer {"some-referrer" 12345}})
-
+  (println "u" url "r" referrer)
+  (r/fold
+   (r/monoid #(deep-merge-with + %1 %2) (constantly {}))
+   (r/map #(solution-file url referrer %1)
+          (filter #(.isFile %1) (file-seq (io/as-file *logs-dir*)))))
+;;   {:total-bytes      12345
+;;    ;; если указан параметр url, то в хэш-мапе будет только одно значение
+;;    :bytes-by-url     {"some-url" 12345}
+;;    ;; если указан параметр referrer, то в хэш-мапе будет только одно значение
+;;    :urls-by-referrer {"some-referrer" 12345}}
+  )
 
 
 (comment
  ;; возможные вызовы функции
- (solution)
- (solution :url "some-url")
- (solution :referrer "some-referrer")
- (solution :url "some-url" :referrer "some-referrer"))
+  (time (do (solution) (+ 1 1)))
+  (solution)
+  (solution :url "some-url")
+  (solution :referrer "some-referrer")
+  (solution :url "some-url" :referrer "some-referrer"))
--- otus-16/test/otus_16/homework_test.clj
+++ otus-16/test/otus_16/homework_test.clj
@@ -1,6 +1,61 @@
 (ns otus-16.homework-test
   (:require [clojure.test :refer :all]
-            [otus-16.core :refer :all]))
+            [otus-16.homework :as sut]))
 
+(def line1 "18.217.223.118 - - [19/Jul/2020:07:14:55 +0000] \"GET /123 HTTP/1.1\" 301 178 \"https://baneks.site/\" \"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko\" \"-\"")
 
+(def lines ["18.217.223.118 - - [19/Jul/2020:07:14:55 +0000] \"GET / HTTP/1.1\" 301 178 \"-\" \"Mozilla/5.0\" \"-\""
+            "46.229.168.142 - - [19/Jul/2020:07:41:35 +0000] \"GET /%D0%BE%D0/ HTTP/1.1\" 200 8302 \"-\" \"Mozilla/5.0\" \"-\""
+            "176.59.98.98 - - [19/Jul/2020:07:42:32 +0000] \"GET /%D0%B4%D0/ HTTP/2.0\" 200 13797 \"https://baneks.site/\" \"Mozilla/5.0\" \"-\""])
 
+(deftest test-url-decode
+  (testing
+   (is (= "/" (sut/url-decode "/")))
+    (is (= "/оо" (sut/url-decode "/%D0%BE%D0%BE")))
+    (is (= "/уч" (sut/url-decode "/\\xD1\\x83\\xD1\\x87")))))
+
+(deftest test-extract-url
+  (testing
+   (is (= "/" (sut/extract-url "GET / HTTP/1.1")))
+    (is (= "/123" (sut/extract-url "/123")))))
+
+(deftest test-parse-line
+  (testing
+   (is (= {:bytes-by-url {"/123" 178}, :total-bytes 178, :urls-by-referrer {"baneks.site" 1}}
+          (sut/parse-line line1 "/123" "baneks.site")))))
+
+(deftest test-read-file
+  (testing
+   (is (= 3 (sut/lines-of-file "test/otus_16/test-logs/file1.txt" count)))))
+
+(deftest test-count-bytes-use-redusers
+  (testing
+   (is (= {:bytes-by-url {"/" 178}, :total-bytes 22277, :urls-by-referrer {"-" 2, "https://baneks.site/" 1}}
+          (sut/count-bytes-use-redusers lines "/" :all)))))
+
+(deftest count-bytes-use-pmap
+  (testing
+   (is (= {:bytes-by-url {"/" 178}, :total-bytes 22277, :urls-by-referrer {"-" 2, "https://baneks.site/" 1}}
+          (sut/count-bytes-use-pmap lines "/" :all)))))
+
+(deftest test-solution-file
+  (testing
+   (is (= {:bytes-by-url {"/" 178}, :total-bytes 22277, :urls-by-referrer {"-" 2}})
+       (sut/solution-file "/" "baneks.site" "test/otus_16/test-logs/file1.txt"))))
+
+(deftest test-deep-merge-with
+  (testing
+   (is (=  {:bytes-by-url {"/" 356}, :total-bytes 356, :urls-by-referrer {"-" 2}}
+           (sut/deep-merge-with +
+                                {:bytes-by-url {"/" 178}, :total-bytes 178, :urls-by-referrer {"-" 1}}
+                                {:bytes-by-url {"/" 178}, :total-bytes 178, :urls-by-referrer {"-" 1}})))))
+
+(deftest test-solution
+  (binding [sut/*logs-dir* "test/otus_16/test-logs"]
+    (testing
+     (is (= 36236 (:total-bytes (sut/solution))))
+      (is (= 178 (get-in (sut/solution :url "/") [:bytes-by-url "/"])))
+      (is (= 2 (get-in (sut/solution :url "/" :referrer "baneks.site") [:urls-by-referrer "baneks.site"]))))))
+
+(comment
+  (run-tests *ns*))
