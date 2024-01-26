aa21a9b63ae92fcb9baa411e09c9230fe89484ef
Ivan <skylightis666@gmail.com>
2024-01-27 01:34:25 +0300

    HW otus-14 it's look like ok

--- otus-14/homework/src/spec_faker/core.clj
+++ otus-14/homework/src/spec_faker/core.clj
@@ -1,5 +1,5 @@
 (ns spec-faker.core
-    (:require [cheshire.core :refer :all]
+    (:require [cheshire.core :as cheshire]
               [clojure.spec.gen.alpha :as gen]
               [compojure.core :refer :all]
               [hiccup.core :refer [html]]
@@ -7,52 +7,45 @@
               [ring.middleware.json :refer [wrap-json-response]]
               [ring.middleware.keyword-params :refer :all]
               [ring.middleware.params :refer :all]
-              [ring.util.response :refer [response]]
-              [spec-faker.generators :refer [generators]])  ;TODO clean
+              [ring.middleware.resource :refer :all]
+              [ring.util.response :refer [redirect resource-response response]]
+              [spec-faker.generators :refer [generators]])
     (:gen-class))
 
-(comment
-    {:key1 "spec-string"
-     :key2 "spec-int64"
-     :key3 "spec-bool"})
-(def current-spec (atom {}))
+(def example-input (cheshire/encode {:key1 "spec-string"
+                                     :key2 "spec-int64"
+                                     :key3 "spec-bool"}))
 
-(defn change-spec [request]
-    (reset! current-spec (request :body))
-    (response "OK"))
+(defn generate-spec [json-string]
+    (let [json (cheshire/decode json-string)]
+        (response (zipmap (keys json)
+                          (map (comp gen/generate generators) (vals json))))))
 
-(defn generate-spec
-    ([]
-     (response (zipmap (keys @current-spec)
-                       (map (comp gen/generate generators)
-                            (vals @current-spec)))))
-    ([json-string]
-     (clojure.pprint/pprint json-string)
-     (response (zipmap (keys (cheshire.core/decode json-string))
-                       (map (comp gen/generate generators)
-                            (vals (cheshire.core/decode json-string)))))))
+(defmulti spec-get-handler #(nil? (get-in % [:params :spec])))
+
+(defmethod spec-get-handler true [_]
+    (html [:form {:action "/" :method "POST"}
+           [:div
+            [:label {:for "spec"} "Input spec: "]
+            [:input#spec {:name  "spec"
+                          :value example-input
+                          :style "width:500px; height:200px"}]
+            ]
+           [:button.btn.btn-primary {:type "submit"} "Submit"]]))
+
+(defmethod spec-get-handler false [request]
+    (generate-spec (get-in request [:params :spec])))
+
 (defroutes handler
-           (context "/spec" []
-               (GET "/" request (generate-spec (get-in request [:params :say])))
-               (POST "/" request (change-spec request))
-               (GET "/html-test" _ (html [:form {:action "http://localhost:8000/spec" :method "GET"}
-                                          [:div
-                                           [:label {:for "say"} "What greeting do you want to say?"]
-                                           [:input#say {:name "say" :value "Hi"}]]
-                                          [:button.btn.btn-primary {:type "submit"}
-                                           "Submit"]]))))
-(defn wrap-print-response [handler]
-    (fn
-        ([request]
-         (print request)
-         (handler request))))
+           (GET "/" request (spec-get-handler request))
+           (POST "/" request (redirect (str "?spec=" (get-in request [:params :spec]))))
+           (GET "/favicon.ico" _ (resource-response "public/favicon.ico")))
+
 (def app (-> handler
-             ;wrap-json-body
+             (wrap-resource "public")
              wrap-json-response
-             ;wrap-json-params
              wrap-keyword-params
              wrap-params
-             ;wrap-print-response
              ))
 
 (def server (run-jetty #'app {:port 8000 :join? false}))
