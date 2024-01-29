8cc5f7b42d5ac112d4eecfbed77755c56e36e89d
Vadzimz <56486120+Vadzimz@users.noreply.github.com>
2024-01-29 18:10:41 +0300

    Update homework 10.clj

--- otus-10/src/otus_10/homework.clj
+++ otus-10/src/otus_10/homework.clj
@@ -1,10 +1,10 @@
-(ns otus-10-homework.core
-(:require [clojure.string :as string])
-  (:require [clojure.java.io :as io]))
+(ns otus-10-homework
+  (:require [clojure.string :as string])
+   (:require [clojure.java.io :as io]))
 
 (def n1 "file-12926-ed090b.mp3")
 
-(defn check-flag 
+(defn check-flag
   "Проверяем n-ый бит байта в виде integer"
   [num-int ind]
   (->> num-int
@@ -13,7 +13,7 @@
        (#(nth % ind \0))
        (= \1)))
 
-(defn get-size 
+(defn get-size
   "Получаем размер (header, subheader or frame)"
   [arr]
   (->> (range (count arr))
@@ -22,41 +22,38 @@
        (map * arr)
        (apply +)))
 
-(defmulti parse-frame first)
-(defmethod parse-frame 0
-  [coll] (String. (byte-array (remove zero? (next coll))) "Windows-1252"))
-(defmethod parse-frame 1
-  [coll] (String. (byte-array (remove zero? (next coll))) "UTF-16"))
-(defmethod parse-frame 2
-  [coll] (String. (byte-array (remove zero? (next coll))) "UTF-16BE"))
-(defmethod parse-frame 3
-  [coll] (String. (byte-array (remove zero? (next coll))) "UTF-8"))
 
-;; вариант функции выбора кодировки без case
-(defn parse-frame-v2 [coll]
+(defn parse-frame-text [coll]
   (let [[encoding-byte & rest] coll
         encoding-types ["Windows-1252" "UTF-16" "UTF16BE" "UTF-8"]]
     (String. (byte-array rest) (nth encoding-types encoding-byte))))
 
-(defn get-frame 
+(defn get-frame-inside
   "парсим один фрейм"
   [stream]
-  (let [[nm1 nm2 nm3 nm4 sz1 sz2 sz3 sz4 _ _ & rest] stream
-        name (apply str (map char [nm1 nm2 nm3 nm4]))
+  (let [[_ _ _ _ sz1 sz2 sz3 sz4 _ _ & rest] stream
         size (get-size [sz1 sz2 sz3 sz4])
-        text (parse-frame (take size rest))]
-    {:name name :size size :text text}))
+        text (parse-frame-text (take size rest))]
+    {:size size :text text}))
 
+(defmulti get-frame (fn [coll] (apply str (map char (take 4 coll)))))
+
+(defmethod get-frame "TALB" [coll] (assoc (get-frame-inside coll) :name "TALB" :description "Album/Movie/Show title"))
+(defmethod get-frame "TYER" [coll] (assoc (get-frame-inside coll) :name "TYER" :description "Year"))
+(defmethod get-frame "TCON" [coll] (assoc (get-frame-inside coll) :name "TCON" :description "Content type"))
+(defmethod get-frame "TDRC" [coll] (assoc (get-frame-inside coll) :name "TDRC" :description "Recording time"))
+(defmethod get-frame "TIT2" [coll] (assoc (get-frame-inside coll) :name "TIT2" :description "Title/songname/content description"))
+(defmethod get-frame "TPE1" [coll] (assoc (get-frame-inside coll) :name "TPE1" :description "Lead performer(s)/Soloist(s)"))
 
 (defn parse-mp3-tags [file]
-(with-open [in (io/input-stream (io/file (io/resource file)))]
-  (let [[_ _ _ _ _ flag-byte sz1 sz2 sz3 sz4 & rest] (.readAllBytes in)
-        tags-size (get-size [sz1 sz2 sz3 sz4])
-        [x1 x2 x3 x4] rest
-        sub-header-size (if (check-flag flag-byte 6) (get-size [x1 x2 x3 x4]) 0)
-        frames (drop sub-header-size (take tags-size rest))]
-    (loop [rem-frames frames
-           acc []]
-      (if (or (empty? rem-frames) (zero? (first rem-frames))) acc
-          (recur (drop (+ 10 (:size (get-frame rem-frames))) rem-frames)
-                 (conj acc (get-frame rem-frames))))))))
+  (with-open [in (io/input-stream (io/file (io/resource file)))]
+    (let [[_ _ _ _ _ flag-byte sz1 sz2 sz3 sz4 & rest] (.readAllBytes in)
+          tags-size (get-size [sz1 sz2 sz3 sz4])
+          [x1 x2 x3 x4] rest
+          sub-header-size (if (check-flag flag-byte 6) (get-size [x1 x2 x3 x4]) 0)
+          frames (drop sub-header-size (take tags-size rest))]
+      (loop [rem-frames frames
+             acc []]
+        (if (or (empty? rem-frames) (zero? (first rem-frames))) acc
+            (recur (drop (+ 10 (:size (get-frame rem-frames))) rem-frames)
+                   (conj acc (get-frame rem-frames))))))))
