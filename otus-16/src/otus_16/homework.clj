7736ecbace07808df248ca296571a644df91797e
Ivan <skylightis666@gmail.com>
2024-02-04 00:42:03 +0300

    HW otus-16 first try with pmap + atom

--- otus-16/src/otus_16/homework.clj
+++ otus-16/src/otus_16/homework.clj
@@ -1,21 +1,53 @@
 (ns otus-16.homework)
 
+(def parser-state (atom {:total-bytes      0
+                         :bytes-by-url     0
+                         :urls-by-referrer 0}))
 
+;(defn parallel-read
+;    [^String file-path start-position end-position]
+;    (with-open [file (RandomAccessFile. file-path "r")]
+;        (.seek file start-position)
+;        (let [byte-array (byte-array (- end-position start-position))
+;              _ (.readFully file byte-array)]
+;            ;; convert byte-array to string or process as needed
+;            )))
+
+(defn parse-log-line [line]
+    (let [size (-> line
+                   (clojure.string/replace #"\s\[.*?\]\s" " ")
+                   (clojure.string/replace #"\s\".*?\"\s" " ")
+                   (clojure.string/split #"\s+")
+                   (nth 4)
+                   parse-long
+                   )]
+        (swap! parser-state update :total-bytes (partial + size))))
+
+(comment
+    (parse-log-line "asd asd asf asfas ffga 12 123")
+    (parse-log-line "asd asd asf asfas ffga 12 677")
+    (reset! parser-state {:total-bytes      0
+                          :bytes-by-url     0
+                          :urls-by-referrer 0}))
+
+(defn reset-state []
+    (reset! parser-state {:total-bytes      0
+                          :bytes-by-url     0
+                          :urls-by-referrer 0}))
 
 (defn solution [& {:keys [url referrer]
                    :or   {url :all referrer :all}}]
-  (println "doing something")
-  {:total-bytes      12345
-   ;; если указан параметр url, то в хэш-мапе будет только одно значение
-   :bytes-by-url     {"some-url" 12345}
-   ;; если указан параметр referrer, то в хэш-мапе будет только одно значение
-   :urls-by-referrer {"some-referrer" 12345}})
-
+    (with-open [rdr (clojure.java.io/reader "logs/access.log.2")]
+        (doall (pmap parse-log-line (line-seq rdr))))
+    (let [result @parser-state]
+        (reset-state)
+        result))
 
 
 (comment
- ;; возможные вызовы функции
- (solution)
- (solution :url "some-url")
- (solution :referrer "some-referrer")
- (solution :url "some-url" :referrer "some-referrer"))
+    ;; возможные вызовы функции
+    (solution)
+    @parser-state
+    (solution :url "some-url")
+    (solution :referrer "some-referrer")
+    (solution :url "some-url" :referrer "some-referrer"))
