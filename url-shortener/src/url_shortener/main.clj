f8048243053ca239dd51670f5473b839618fb4c5
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2024-02-08 16:37:02 +0300

    URL shortener: clean up main

--- url-shortener/src/url_shortener/main.clj
+++ url-shortener/src/url_shortener/main.clj
@@ -21,26 +21,3 @@
     (.addShutdownHook
      (Runtime/getRuntime)
      (Thread. ^Runnable #(stop-server server)))))
-
-(comment
-  (def server (start-server {:port 8000 :join? false}))
-  (stop-server server)
-  
-  (require '[clojure.java.shell :refer [sh]])
-  
-  (sh "curl" "-X" "POST"
-      "-H" "Content-Type: application/json"
-      "http://localhost:8000/"
-      "-d" "{\"url\": \"https://clojurescript.org/\"}")
-  
-  (sh "curl" "-X" "PUT"
-      "-H" "Content-Type: application/json"
-      "http://localhost:8000/clj"
-      "-d" "{\"url\": \"https://clojure.org/\"}")
-  
-  (sh "curl" "-i" "http://localhost:8000/clj")
-  
-  (sh "curl" "http://localhost:8000/list/")
-  )
-
-
