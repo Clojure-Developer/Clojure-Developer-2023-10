55354bb50abd3ff7c19905923ab9fd1e547a0b46
Ivan <skylightis666@gmail.com>
2024-02-22 00:47:30 +0300

    HW otus-16 removed atoms + some refactoring

--- otus-16/src/otus_16/homework.clj
+++ otus-16/src/otus_16/homework.clj
@@ -3,16 +3,9 @@
 
 (def log_indices {:ip 1 :identifier 2 :userid 3 :time 4 :request 5 :status 6 :size 7 :referrer 8 :user-agent 9})
 
-(def start-number-state {:total-bytes 0 :bytes-per-url 0})
-(def start-set-state {:urls-per-referrer #{}})
-
-(def number-state (atom start-number-state))
-
-(def set-state (atom start-set-state))
-
-(defn reset-state []
-    (reset! number-state start-number-state)
-    (reset! set-state start-set-state))
+(def start-result-state {:total-bytes       0
+                         :bytes-per-url     0
+                         :urls-per-referrer #{}})
 
 (defn- parse-request [request]
     (let [[method url http-version] (clojure.string/split request #" ")]
@@ -23,65 +16,65 @@
           matcher (re-find regex log-line)]
         (if matcher
             (let [parsed-request (parse-request (nth matcher (log_indices :request)))]
-                (merge
-                    {:ip         (nth matcher (log_indices :ip))
-                     :identifier (nth matcher (log_indices :identifier))
-                     :userid     (nth matcher (log_indices :userid))
-                     :time       (nth matcher (log_indices :time))
-                     :status     (nth matcher (log_indices :status))
-                     :size       (parse-long (nth matcher (log_indices :size)))
-                     :referrer   (nth matcher (log_indices :referrer))
-                     :user-agent (nth matcher (log_indices :user-agent))}
-                    {:request parsed-request}))
+                {:ip         (nth matcher (log_indices :ip))
+                 :identifier (nth matcher (log_indices :identifier))
+                 :userid     (nth matcher (log_indices :userid))
+                 :time       (nth matcher (log_indices :time))
+                 :status     (nth matcher (log_indices :status))
+                 :size       (parse-long (nth matcher (log_indices :size)))
+                 :referrer   (nth matcher (log_indices :referrer))
+                 :user-agent (nth matcher (log_indices :user-agent))
+                 :request    parsed-request})
             :no-match)))
 
+(defn parse-logs-portion [url referrer log-lines]
+    (let [parsed-lines (map parse-apache-log log-lines)]
+        (reduce
+            (fn [acc parsed-line]
+                (let [bytes (:size parsed-line)
+                      line-url (:url (:request parsed-line))
+                      line-referrer (:referrer parsed-line)
+                      total-bytes (+ (acc :total-bytes) bytes)
+                      bytes-per-url (if (or (= url line-url) (= url :all))
+                                        (+ (:bytes-per-url acc) bytes)
+                                        (:bytes-per-url acc))
+                      urls-per-referrer (if (or (= referrer line-referrer) (= referrer :all))
+                                            (conj (:urls-per-referrer acc) line-url)
+                                            (:urls-per-referrer acc))]
+                    {:total-bytes       total-bytes
+                     :bytes-per-url     bytes-per-url
+                     :urls-per-referrer urls-per-referrer}))
+            start-result-state
+            parsed-lines)))
 
-(defn size-reducer [lines]
-    (reduce #(+ %1 (:size %2)) 0 lines))
 
-(defn has-url? [url]
-    (fn [el]
-        (= (:url (:request el)) url)))
+(def merge-results
+    (partial merge-with (fn [x y]
+                            (if (set? x)
+                                (set/union x y)
+                                (+ x y)))))
 
-(defn has-referrer? [referrer]
-    (fn [el]
-        (= (:referrer el) referrer)))
-
-(defn parse&persist-logs-portion [url referrer log-lines]
-    (let [parsed-lines (map parse-apache-log log-lines)
-          modified-states (reduce
-                              (fn [{:keys [total-numbers set-states]} parsed-line]
-                                  (let [bytes (:size parsed-line)
-                                        line-url (:url (:request parsed-line))
-                                        line-referrer (:referrer parsed-line)
-                                        total-numbers (update total-numbers :total-bytes + bytes)
-                                        total-numbers (if (or (= url line-url) (= url :all))
-                                                          (update total-numbers :bytes-per-url + bytes)
-                                                          total-numbers)
-                                        set-states (if (or (= referrer line-referrer) (= referrer :all))
-                                                       (update set-states :urls-per-referrer conj line-url)
-                                                       set-states)]
-                                      {:total-numbers total-numbers, :set-states set-states}))
-                              {:total-numbers start-number-state, :set-states start-set-state}
-                              parsed-lines)]
-        (swap! number-state #(merge-with + % (:total-numbers modified-states)))
-        (swap! set-state #(merge-with set/union % (:set-states modified-states)))))
+(defn reduce-results [coll]
+    (doall (reduce merge-results start-result-state coll)))
 
 (defn parallel-parse-file [url referrer file]
     (with-open [rdr (clojure.java.io/reader file)]
-        (doall (pmap (partial parse&persist-logs-portion url referrer) (partition-all 100000 (line-seq rdr))))))
+        (->> rdr
+             (line-seq)
+             (partition-all 100000)
+             (pmap (partial parse-logs-portion url referrer))
+             (reduce-results)
+             (doall))))
+
 
 (defn solution [& {:keys [url referrer]
                    :or   {url :all referrer :all}}]
     (let [files (->> "logs"
                      clojure.java.io/file
                      file-seq
-                     (filter #(.isFile %)))]
-        (doall (pmap (partial parallel-parse-file url referrer) files)))
-    (let [result1 @number-state
-          result2 @set-state]                               ; TODO closure with atom?
-        (reset-state)
-        (merge result1 {:urls-per-referrer (count (:urls-per-referrer result2))})))
+                     (filter #(.isFile %)))
+          result-with-urls (reduce-results (pmap (partial parallel-parse-file url referrer) files))]
+        (update result-with-urls :urls-per-referrer count)))
 
 
 (comment
